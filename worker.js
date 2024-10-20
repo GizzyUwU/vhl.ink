25d2cf72259dd8d421dd595668c00e993819f58f
GizzyUwU <nerop455@gmail.com>
2024-10-20 16:05:25 +0000

    Testing

--- worker.js
+++ worker.js
@@ -87,7 +87,7 @@ async function handleRequest(request, event) {
 		if (psk === SECRET_KEY) {
 			const { keys } = await LINKS.list();
 			let paths = "";
-			keys.forEach(element => paths += `${element.name}\n`);
+			keys.forEach(element => paths += `${element}\n`);
 			
 			return new Response(paths, { status: 200 });
 		}
